f04f44dba4f11172a31c1ab6e7e6025bdd2953a8
s18684 <s18684@pjwstk.edu.pl>
2024-04-16 12:25:53 +0200

    Added Visit class, updated animals controller with methods and database simulation, Task finished

--- Cwiczenia 4/Controllers/AnimalsController.cs
+++ Cwiczenia 4/Controllers/AnimalsController.cs
@@ -9,9 +9,16 @@ public class AnimalsController : ControllerBase
 {
     private static readonly List<Animal> _animals = new()
     {
-        new Animal { category = "Dog", hairColor = "Brown", id = 1, mass = 50.0, name = "Dong" },
-        new Animal { category = "Cat", hairColor = "White", id = 2, mass = 10.0, name = "Kat" },
-        new Animal { category = "Bird", hairColor = "Pink", id = 3, mass = 5.0, name = "BirdIsAWord" }
+        new Animal { Category = "Dog", HairColor = "Brown", AnimalId = 1, Mass = 50.0, Name = "Dong"},
+        new Animal { Category = "Cat", HairColor = "White", AnimalId = 2, Mass = 10.0, Name = "Kat" },
+        new Animal { Category = "Bird",HairColor = "Pink", AnimalId = 3, Mass = 5.0, Name = "BirdIsAWord"}
+    };
+    
+    private static List<Visit> _visits = new()
+    {
+        new Visit{AnimalId = 1, Cost = 50.0, Description = "Tooth removal", IdVisit = 1, VisitDate = DateTime.Parse("2024-04-10")},
+        new Visit{AnimalId = 2, Cost = 10.0, Description = "Claws clipping", IdVisit = 2, VisitDate = DateTime.Parse("2023-04-10")},
+        new Visit{AnimalId = 3, Cost = 100.0, Description = "Beak sharpening", IdVisit = 3, VisitDate = DateTime.Parse("2024-03-10")}
     };
     
     [HttpGet]
@@ -23,7 +30,7 @@ public class AnimalsController : ControllerBase
     [HttpGet("{id:int}")]
     public IActionResult GetStudent(int id)
     {
-        var animal = _animals.FirstOrDefault(a => a.id == id);
+        var animal = _animals.FirstOrDefault(a => a.AnimalId == id);
         if (animal == null)
         {
             return NotFound($"Animal with id {id} was not found");
@@ -42,7 +49,7 @@ public class AnimalsController : ControllerBase
     [HttpPut("{id:int}")]
     public IActionResult UpdateAnimal(int id, Animal animal)
     {
-        var animalToEdit = _animals.FirstOrDefault(a => a.id == id);
+        var animalToEdit = _animals.FirstOrDefault(a => a.AnimalId == id);
 
         if (animalToEdit == null)
         {
@@ -57,7 +64,7 @@ public class AnimalsController : ControllerBase
     [HttpDelete("{id:int}")]
     public IActionResult DeleteAnimal(int id)
     {
-        var animalToEdit = _animals.FirstOrDefault(a => a.id == id);
+        var animalToEdit = _animals.FirstOrDefault(a => a.AnimalId == id);
 
         if (animalToEdit == null)
         {
@@ -66,6 +73,28 @@ public class AnimalsController : ControllerBase
 
         _animals.Remove(animalToEdit);
         return NoContent();
+    }
+    
+    [HttpGet("{id:int}/visits")]
+    public IActionResult GetAnimalVisits(int id)
+    {
+        var animalVisits = _visits.Where(v => v.AnimalId == id).ToList();
+        return Ok(animalVisits);
+    }
+    
+    [HttpPost("{id:int}/visits")]
+    public IActionResult CreateVisit(int id, Visit visit)
+    {
+        var animal = _animals.FirstOrDefault(a => a.AnimalId == id);
+        if (animal == null)
+        {
+            return NotFound($"Animal with id {id} was not found");
+        }
+
+        visit.AnimalId = id;
+        
+        _visits.Add(visit);
+        return StatusCode(StatusCodes.Status201Created);
     }
     
     
--- Cwiczenia 4/Models/Animal.cs
+++ Cwiczenia 4/Models/Animal.cs
@@ -2,9 +2,10 @@ namespace Cwiczenia_4.Models;
 
 public class Animal
 {
-    public int id { get; set; }
-    public string name{ get; set; }
-    public string category{ get; set; }
-    public double mass{ get; set; }
-    public string hairColor{ get; set; }
+    public int AnimalId { get; set; }
+    public string Name{ get; set; }
+    public string Category{ get; set; }
+    public double Mass{ get; set; }
+    public string HairColor{ get; set; }
+
 }
